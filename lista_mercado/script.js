fea524cf0332a1b3a6c3ce07417db6636ae32d4f
João Victor Oliveira <joao.m.oliveira@sesisc.org.br>
2020-12-02 10:45:26 -0300

    ex. lista mercado - primeira versao: adiciona na lista, salva e carrega do local storage

---
+++ lista_mercado/script.js
@@ -0,0 +1,46 @@
+var btnAdd = document.getElementById('btnAdd');
+var txtItem = document.getElementById('txtItem');
+var selLista = document.getElementById('selLista');
+var listaMercado = [];
+carregarListaMercado();
+
+function adicionarNaLista() {
+    var itemMercado = txtItem.value;
+    if (itemMercado) {
+        criarElementoLista(itemMercado);
+        salvarNoLocalStorage(itemMercado);
+    } else {
+        alert("Favor digitar algo no campo de texto.");
+    }
+}
+
+function salvarNoLocalStorage(item) {
+    listaMercado.push(item);
+    localStorage.setItem('listaMercado', JSON.stringify(listaMercado));
+}
+
+function criarElementoLista(itemText) {
+    var optItem = document.createElement('option'); // <option></option>
+    optItem.textContent = itemText; // <option>itemMercado</option>
+    selLista.appendChild(optItem);
+}
+
+function carregarListaMercado() {
+    var teste = JSON.parse(localStorage.getItem('listaMercado'));
+    if (teste) {
+        listaMercado = teste;
+        for (var i = 0; i < listaMercado.length; i++) {
+            criarElementoLista(listaMercado[i]);
+        }
+    }
+}
+
+function verificaTecla(event) {
+    if (event.key == 'Enter') {
+        adicionarNaLista();
+    }
+}
+
+btnAdd.addEventListener('click', adicionarNaLista);
+
+txtItem.addEventListener('keyup', verificaTecla);
